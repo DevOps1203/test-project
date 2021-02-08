204d3dfba64518cb9b2ef587e6722ff91ac3a0f2
DevOps1203 <78731023+DevOps1203@users.noreply.github.com>
2021-02-08 16:38:07 +0530

    Add files via upload

---
+++ FizzBuzz.java
@@ -0,0 +1,26 @@
+package com.git.exmaple;
+
+public class FizzBuzz {
+
+	public static void main(String[] args) {
+
+
+			int n = 100;
+			for(int i = 0; i<=n; i++) {
+				
+				if(i%3==0 && i%5==0) {
+					System.out.println("FizzBuzz");
+				}
+				else if(i%3==0) {
+					System.out.println("Fizz");
+				}
+				else if(i%5==0) {
+					System.out.println("Buzz");
+				}
+				else {
+					System.out.println("i");
+				}
+			}
+	}
+}
+	
